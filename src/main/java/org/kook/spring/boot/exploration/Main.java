6231c743f1a8daa08615acaac4e26d6a04753b04
andrii.kook <102363876+andrii-k6a@users.noreply.github.com>
2022-10-20 14:03:15 +0300

    introduce spring boot

--- src/main/java/org/kook/spring/boot/exploration/Main.java
+++
@@ -1,7 +0,0 @@
-package org.kook.spring.boot.exploration;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/org/kook/spring/boot/exploration/SimpleApplicationTest.java
@@ -0,0 +1,12 @@
+package org.kook.spring.boot.exploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTest {
+
+    @Test
+    void contextLoads() {}
+
+}
